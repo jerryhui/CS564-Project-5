61bee122d0a9bb80f9a47678539663c07ebd0110
Jerry Hui <chiwei.hui@gmail.com>
2012-11-12 14:19:12 -0600

    AttrCatalog::getInfo first implementation

--- catalog.C
+++ catalog.C
@@ -60,21 +60,58 @@ AttrCatalog::AttrCatalog(Status &status) :
 }
 
 
-const Status AttrCatalog::getInfo(const string & relation, 
+/***
+ * Returns the attribute descriptor record for attribute attrName in relation relName. Uses 
+ * a scan over the underlying heapfile to get all tuples for relation and check each tuple 
+ * to find whether it corresponds to attrName. (Or maybe do it the other way around !) This has 
+ * to be done this way because a predicated HeapFileScan does not allow conjuncted predicates. 
+ * Note that the tuples in attrcat are of type AttrDesc (structure given above).
+ *
+ * 11/12/2012 JH: First implementation.
+ ***/
+const Status AttrCatalog::getInfo(const string & relation,
 				  const string & attrName,
 				  AttrDesc &record)
 {
 
-  Status status;
-  RID rid;
-  Record rec;
-  HeapFileScan*  hfs;
+    Status status;
+    RID rid;
+    Record rec;
+    HeapFileScan*  hfs;
+    
+    AttrDesc* iDesc;
+    bool found=false;
 
-  if (relation.empty() || attrName.empty()) return BADCATPARM;
+    if (relation.empty() || attrName.empty()) return BADCATPARM;
+    
+    // open AttrCatalog file
+    hfs = new HeapFileScan(ATTRCATNAME, status);
+    if (status==OK) {
+        if ((status = hfs->startScan(0, 0, STRING, "", EQ))==OK) {
 
-
-
-
+            // scan for requested relation
+            while (!found && status != FILEEOF) {
+                status = hfs->scanNext(rid);
+                if (status!=OK) break;
+                
+                if ( (status = hfs->HeapFile::getRecord(rid, rec))==OK ) {
+                    iDesc = (AttrDesc*)rec.data;
+                    
+                    // if found, return relation                    
+                    if ( attrName.compare(iDesc->attrName)==0
+                        && relation.compare(iDesc->relName)==0 ) {
+                        record = *iDesc;
+                        found = true;
+                    }
+                }
+            }
+            
+        }
+    }
+    
+    // clear memory
+    delete hfs;
+    return status;
 }
 
 
