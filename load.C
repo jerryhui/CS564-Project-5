e65185ea90e6b14af92b54984306530a55804343
Jerry Hui <chiwei.hui@gmail.com>
2012-11-14 14:01:04 -0600

    UT_Load first implementation

--- load.C
+++ load.C
@@ -4,69 +4,72 @@
 #include "utility.h"
 #include "heapfile.h"
 
-//
-// Loads a file of (binary) tuples from a standard file into the relation.
-// Any indices on the relation are updated appropriately.
-//
-// Returns:
-// 	OK on success
-// 	an error code otherwise
-//
-
+/***
+ * Loads a file of (binary) tuples from a standard file into the relation.
+ * Any indices on the relation are updated appropriately.
+ *
+ * Returns:
+ *  OK on success
+ *  an error code otherwise
+ *
+ * 2012/11/14 JH: First implementation.
+ ***/
 const Status UT_Load(const string & relation, const string & fileName)
 {
-  Status status;
-  RelDesc rd;
-  AttrDesc *attrs;
-  int attrCnt, i;
-  InsertFileScan * iFile;
-  int width = 0;
-  int records = 0;
-  if (relation.empty() || fileName.empty() || relation == string(RELCATNAME)
+    Status status;
+    RelDesc rd;
+    AttrDesc *attrs;
+    int attrCnt, i;
+    InsertFileScan * iFile;
+    int width = 0;
+    int records = 0;
+    if (relation.empty() || fileName.empty() || relation == string(RELCATNAME)
       || relation == string(ATTRCATNAME))
     return BADCATPARM;
 
-  // open Unix data file
+    // open Unix data file
 
-  int fd;
-  if ((fd = open(fileName.c_str(), O_RDONLY, 0)) < 0)
+    int fd;
+    if ((fd = open(fileName.c_str(), O_RDONLY, 0)) < 0)
     return UNIXERR;
 
-  // get relation data
+    // get relation data
+    status = relCat->getInfo(relation, rd);
+    if (status != OK) return status;
+    status = attrCat->getRelInfo(relation, attrCnt, attrs);
+    if (status != OK) return status;
+    
+    //  determine length of each record
+    for (i=0; i<attrCnt; i++) {
+        width += attrs[i].attrLen;
+    }
+
+    // start insertFileScan on relation
+    iFile = new InsertFileScan(relation, status);
+    if (status != OK) return status;
+
+    // allocate buffer to hold record read from unix file
+    char *record;
+    if (!(record = new char [width])) return INSUFMEM;
+
+    int nbytes;
+    Record rec;
+
+    // read next input record from Unix file and insert it into relation
+    while((nbytes = (int)read(fd, record, width)) == width) {
+        RID rid;
+        rec.data = record;
+        rec.length = width;
+        if ((status = iFile->insertRecord(rec, rid)) != OK) return status;
+        records++;
+    }
+
+    // close heap file and unix file
+    if (close(fd) < 0) return UNIXERR;
 
 
 
-
-  // start insertFileScan on relation
-
-
-
-
-
-
-
-
-  // allocate buffer to hold record read from unix file
-  char *record;
-  if (!(record = new char [width])) return INSUFMEM;
-
-  int nbytes;
-  Record rec;
-
-  // read next input record from Unix file and insert it into relation
-  while((nbytes = read(fd, record, width)) == width) {
-    RID rid;
-    rec.data = record;
-    rec.length = width;
-    if ((status = iFile->insertRecord(rec, rid)) != OK) return status;
-    records++;
-  }
-
-  // close heap file and unix file
-  if (close(fd) < 0) return UNIXERR;
-
-
-
-
+    delete iFile;
+    return OK;
 }
 
