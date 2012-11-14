6b20e695e9e80e8768c7c8be414126bd5fff5cad
Jerry Hui <chiwei.hui@gmail.com>
2012-11-14 12:38:01 -0600

    AttrCatalog::dropRelation first implementation

--- destroy.C
+++ destroy.C
@@ -27,28 +27,35 @@ const Status RelCatalog::destroyRel(const string & relation)
 }
 
 
-//
-// Drops a relation. It performs the following steps:
-//
-// 	removes the catalog entries for the relation
-//
-// Returns:
-// 	OK on success
-// 	error code otherwise
-//
-
+/***
+ *
+ * Drops a relation. It performs the following steps:
+ *
+ * removes the catalog entries for the relation
+ * Returns:
+ *  OK on success
+ *  error code otherwise
+ *
+ * 2012/11/14 JH: First implementation.
+ ***/
 const Status AttrCatalog::dropRelation(const string & relation)
 {
-  Status status;
-  AttrDesc *attrs;
-  int attrCnt, i;
+    Status status;
+    AttrDesc *attrs;
+    int attrCnt, i;
 
-  if (relation.empty()) return BADCATPARM;
+    if (relation.empty()) return BADCATPARM;
 
-
-
-
-
+    // Obtain info of all atrributes first
+    status = getRelInfo(relation, attrCnt, attrs);
+    if (status != OK) return status;
+    
+    // for each attribute, remove attribute
+    for (i=0; i<attrCnt; i++) {
+        status = removeInfo(relation, attrs[i].attrName);
+    }
+    
+    return status;
 }
 
 
