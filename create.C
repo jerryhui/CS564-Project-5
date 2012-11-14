3cf05fc15145d230e64ffd077267cdef41ceeb20
David Mateo <mateo@wisc.edu>
2012-11-14 13:05:41 -0600

    first attempt at relCat::createRel()

--- create.C
+++ create.C
@@ -15,10 +15,38 @@ const Status RelCatalog::createRel(const string & relation,
   if (relation.length() >= sizeof rd.relName)
     return NAMETOOLONG;
 
-
-
-
-
+  /*  First make sure that a relation with the same name doesn't already exist (by using the getInfo() function described below). Next add a tuple to the relcat relation. Do this by filling in an instance of the RelDesc structure above and then invoking the RelCatalog::addInfo() method. Third, for each of the attrCnt attributes, invoke the AttrCatalog::addInfo() method of the attribute catalog table (remember that this table is referred to by the global variable attrCat), passing the appropriate attribute information from the attrList[] array as an instance of the AttrDesc structure (see below). Finally, create a HeapFile instance to hold tuples of the relation (hint: there is a procedure to do this which we have seen in the last project stage; you need to give it a string that is the relation name). Implement this function in create.C
+   */
+    
+    status = RELCATNAME->getInfo(relation, rd);
+    if (status != OK && status != RELNOTFOUND) {return status;}
+    
+    if (status == OK) {return RELEXISTS;}
+    
+    //RELNOTFOUND so continue creating relation.
+    
+    rd.relName = relation;
+    rd.attrCnt = attrCnt;
+    
+    status = RELCATNAME->addInfo(rd);
+    if (status != OK) {return status;}
+    
+    for(int i; i < attrCnt; i ++)
+    {
+        ad.relName = attrList[i].relName;
+        ad.attrName = attrList[i].attrName;
+        ad.attrOffset = i;
+        ad.attrType = attrList[i].attrType;
+        ad.attrLen = attrList[i].attrLen;
+        
+        status = ATTRCATNAME->addInfo(ad);
+        if(status != OK) {return status;}
+    }
+    
+    status = createHeapFile(relation);
+    if(status != OK) {return status;}
+    
+    else return OK;
 
 }
 
